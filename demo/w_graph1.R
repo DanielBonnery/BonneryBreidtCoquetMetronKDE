69b04f7f8fc646ad56ae6e9938bbef9bf096c581
DanielBonnery <daniel.bonnery@gmail.com>
2017-05-26 13:05:42 -0400

    a

--- demo/w_graph1.R
+++ demo/w_graph1.R
@@ -26,16 +26,19 @@ f2=fHT(y0,Obs,pifun=hatetafunf(xihat=model$xihat,eta=model$eta))
 f3=fHT(y0,Obs,pifun=hatetafunf2)
 
 
-dff<-melt(data.frame(y0=y0,f=f,f1=f1,f2=f2,f3=f3),id="y0")
+dff<-melt(data.frame(y0=y0,f=f,"known xi"=f1,"Estimated xi"=f2,"Eta estimated via parametric methods"=f3),id="y0")
 dff2<-data.frame(y0=y0,value=f,lb=f-qnorm(.975)*sqrt(vf),ub=f+qnorm(.975)*sqrt(vf))
 
 library(ggplot2)
+
 w_graph1.1<-
   ggplot(dff,aes(x=y0,y=value,colour=variable))+
   geom_line()+
   stat_function(fun = function(y){(y>1)*theta/((y+(y==1))^(theta+1))})+
 #  geom_ribbon(data=dff2,aes(ymin=lb, ymax=ub,x=y0), alpha = 0.3,color="gray")+ 
-  ggtitle("One estimation, Pareto")
+  ggtitle("One estimation, Pareto") +
+  theme(legend.justification = c(1, 1), legend.position = c(1, 1))
+
 
 if(dir.exists("datanotpushed")){save(w_graph1.1,file="datanotpushed/w_graph1.1.rda")}
 
