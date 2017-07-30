23c59a926d04e82a8cc2ac62ebf45de432127f00
DanielBonnery <daniel.bonnery@gmail.com>
2017-07-30 06:59:48 -0400

    a

--- R/4-2_kde.R
+++ R/4-2_kde.R
@@ -512,7 +512,7 @@ allplots<-function(ee,scale_colour_function1=ggplot2::scale_colour_grey,scale_co
     xlab("$y_0$")+ylab("")+
     geom_line(size=0.2, alpha=0.1,aes(linetype=joliname,size=.2))+ 
     labs(title="", caption=paste0("Simulations for ",model$name,", and  repeated ",max(nrep,50), " times"))+    
-    geom_line(data=data.frame(y0=y0,f=plyr::aaply(ff[,,"est"],2,mean)),aes(x=y0,y=f,group=NULL,linetype=paste0(joliname,", averaged")),size=1)  +
+    geom_line(data=data.frame(y0=y0,f=plyr::aaply(ff[,,x],2,mean)),aes(x=y0,y=f,group=NULL,linetype=paste0(joliname,", averaged")),size=1)  +
     stat_function(fun = model$dloi.y,size=.4,aes(size=.4,linetype="$f$"))+
     scale_linetype_manual("",values = c("solid",  "solid","dashed")) +
     scale_size_manual(values = c(0.4, .2,1))+
